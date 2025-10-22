687c7388dba72b0656931601743214e16cc3fcb2
Edgar Pfuma <cyber-engineer@outlook.com>
2025-10-22 18:48:14 -0500

    Add Google Analytics global tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
-﻿import { DefaultSeo } from "next-seo";
-import SEO from "../next-seo.config";
+﻿import Script from "next/script";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
-      <DefaultSeo {...SEO} />
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XGDEVCPO8H"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XGDEVCPO8H');
+        `}
+      </Script>
+
+      {/* Your App */}
       <Component {...pageProps} />
     </>
   );
 }
+
+export default MyApp;
