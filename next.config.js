ebc723d2fda77869d1f236cb45837797d7f2d708
Edgar Pfuma <cyber-engineer@outlook.com>
2025-10-23 01:49:25 -0500

    Backup before Notion integration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // ❌ remove or comment out this line if it exists:
-  // output: 'export',
+  output: 'export',
 };
 
 module.exports = nextConfig;
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-﻿import Script from "next/script";
-import "../styles/globals.css";
+﻿import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {/* Google Analytics */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XGDEVCPO8H"
+        src="https://www.googletagmanager.com/gtag/js?id=G-XGDEVCP0BH"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -13,10 +14,11 @@ export default function App({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-XGDEVCPO8H');
+          gtag('config', 'G-XGDEVCP0BH');
         `}
       </Script>
+
       <Component {...pageProps} />
     </>
-  );
+  )
 }
